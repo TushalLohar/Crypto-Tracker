19a12ed46058f69dc22369e1abe61076d685cd43
Tushal Lohar <tushallohar5@gmail.com>
2026-02-26 18:37:06 +0530

    apiclient

---
+++ src/utils/apiClient.jsx
@@ -0,0 +1,79 @@
+const BASE_URL = "https://api.coingecko.com/api/v3";
+const cache = new Map();
+
+export const ApiClient = async (
+  endpoint,
+  { method = "GET", params = {}, signal } = {},
+) => {
+  try {
+    const querystring = new URLSearchParams(params).toString();
+
+    const url = `${BASE_URL}${endpoint}${querystring ? `?${querystring}` : ""}`;
+
+    if (method == "GET" && cache.has(url)) {
+      console.log("serving from cache", url);
+
+      return {
+        data: cache.get(url),
+        error: null,
+        status: 200,
+        fromcache: true,
+      };
+    }
+
+    const response = await fetch(url, { method, signal });
+
+    if (!response.ok) {
+      let errormessage = "An unexpected errro occureed.";
+
+      switch (response.status) {
+        case 400:
+          errormessage = "Bad Request: Check your parameters.";
+          break;
+        case 401:
+          errormessage = "Unauthorized: API key issues.";
+          break;
+        case 404:
+          errormessage = "Not Found: The resource doesn't exist.";
+          break;
+        case 429:
+          errormessage =
+            "Rate Limit Exceeded: Slow down! CoinGecko is blocking us.";
+          break;
+
+        case 500:
+          errormessage = "Server Error: CoinGecko is having trouble.";
+          break;
+      }
+      return {
+        data: null,
+        error: errormessage,
+        status: response.status,
+      };
+    }
+
+    const data = await response.json();
+
+    if (method === "GET") {
+      cache.set(url, data);
+    }
+    return {
+      data,
+      error: null,
+      status: response.status,
+    };
+  } catch (err) {
+    if (err.name === "AbortError") {
+      return {
+        data: null,
+        error: "Request cancelled",
+        isAborted: true,
+      };
+    }
+
+    return {
+      data: null,
+      error: err.message || "Network Error",
+    };
+  }
+};

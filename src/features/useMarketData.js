56da495971511e8d27822d93644867bd934fa897
Tushal Lohar <tushallohar5@gmail.com>
2026-02-28 13:17:25 +0530

    custom hook

---
+++ src/features/useMarketData.js
@@ -0,0 +1,49 @@
+import GlobalMarketData from "../utils/marketAPI";
+import { useState, useEffect } from "react";
+
+export const useMarketData = ({
+  page = 1,
+  perPage = 50,
+  currency = "usd",
+} = {}) => {
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setErrror] = useState(null);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    const fetchCoins = async () => {
+      try {
+        setLoading(true);
+        setErrror(null);
+
+        const { data, error } = await GlobalMarketData({
+          page,
+          perPage,
+          currency,
+          signal: controller.signal,
+        });
+
+        if (error === "Request cancelled") return;
+
+        if (error) {
+          setErrror(error);
+          return;
+        }
+        setCoins(data || []);
+      } catch (err) {
+        if (err.name !== "AbortError") {
+          setErrror("Failed to fetch data");
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCoins();
+    return () => controller.abort();
+  }, [page, perPage, currency]);
+
+  return { coins, loading, error };
+};
